57c58e7c98cd96ba58b23f3665a93382277a003e
cochecheee <buitien747@gmail.com>
2024-04-23 03:24:41 +0700

    add new form use DES enc password store in firebaase

---
+++ demo-des/TrangChu.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace demo_des
+{
+    partial class TrangChu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TrangChu));
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            btn_login = new Button();
+            label4 = new Label();
+            label3 = new Label();
+            txt_password = new TextBox();
+            txt_username = new TextBox();
+            label2 = new Label();
+            tabPage2 = new TabPage();
+            button1 = new Button();
+            label8 = new Label();
+            label5 = new Label();
+            txt_phone = new TextBox();
+            label6 = new Label();
+            txt_pass = new TextBox();
+            txt_name = new TextBox();
+            label7 = new Label();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            btn_logintab = new Button();
+            btn_signuptab = new Button();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Dock = DockStyle.Right;
+            tabControl1.Location = new Point(380, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(730, 635);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.BackColor = Color.AntiqueWhite;
+            tabPage1.Controls.Add(btn_login);
+            tabPage1.Controls.Add(label4);
+            tabPage1.Controls.Add(label3);
+            tabPage1.Controls.Add(txt_password);
+            tabPage1.Controls.Add(txt_username);
+            tabPage1.Controls.Add(label2);
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(722, 602);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Login";
+            // 
+            // btn_login
+            // 
+            btn_login.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_login.Location = new Point(319, 320);
+            btn_login.Name = "btn_login";
+            btn_login.Size = new Size(114, 40);
+            btn_login.TabIndex = 3;
+            btn_login.Text = "Login";
+            btn_login.UseVisualStyleBackColor = true;
+            btn_login.Click += btn_login_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label4.Location = new Point(82, 206);
+            label4.Name = "label4";
+            label4.Size = new Size(73, 20);
+            label4.TabIndex = 2;
+            label4.Text = "Password";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label3.Location = new Point(82, 123);
+            label3.Name = "label3";
+            label3.Size = new Size(79, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Username";
+            // 
+            // txt_password
+            // 
+            txt_password.Location = new Point(202, 203);
+            txt_password.Name = "txt_password";
+            txt_password.Size = new Size(407, 27);
+            txt_password.TabIndex = 1;
+            // 
+            // txt_username
+            // 
+            txt_username.Location = new Point(202, 120);
+            txt_username.Name = "txt_username";
+            txt_username.Size = new Size(407, 27);
+            txt_username.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Verdana", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(319, 48);
+            label2.Name = "label2";
+            label2.Size = new Size(89, 25);
+            label2.TabIndex = 0;
+            label2.Text = "Sign in";
+            // 
+            // tabPage2
+            // 
+            tabPage2.BackColor = Color.AntiqueWhite;
+            tabPage2.Controls.Add(button1);
+            tabPage2.Controls.Add(label8);
+            tabPage2.Controls.Add(label5);
+            tabPage2.Controls.Add(txt_phone);
+            tabPage2.Controls.Add(label6);
+            tabPage2.Controls.Add(txt_pass);
+            tabPage2.Controls.Add(txt_name);
+            tabPage2.Controls.Add(label7);
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(722, 602);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Sign up";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(332, 369);
+            button1.Name = "button1";
+            button1.Size = new Size(114, 40);
+            button1.TabIndex = 9;
+            button1.Text = "Sign up";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label8.Location = new Point(95, 289);
+            label8.Name = "label8";
+            label8.Size = new Size(52, 20);
+            label8.TabIndex = 7;
+            label8.Text = "Phone";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label5.Location = new Point(95, 209);
+            label5.Name = "label5";
+            label5.Size = new Size(73, 20);
+            label5.TabIndex = 7;
+            label5.Text = "Password";
+            // 
+            // txt_phone
+            // 
+            txt_phone.Location = new Point(215, 286);
+            txt_phone.Name = "txt_phone";
+            txt_phone.Size = new Size(407, 27);
+            txt_phone.TabIndex = 5;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label6.Location = new Point(95, 126);
+            label6.Name = "label6";
+            label6.Size = new Size(79, 20);
+            label6.TabIndex = 8;
+            label6.Text = "Username";
+            // 
+            // txt_pass
+            // 
+            txt_pass.Location = new Point(215, 206);
+            txt_pass.Name = "txt_pass";
+            txt_pass.Size = new Size(407, 27);
+            txt_pass.TabIndex = 5;
+            // 
+            // txt_name
+            // 
+            txt_name.Location = new Point(215, 123);
+            txt_name.Name = "txt_name";
+            txt_name.Size = new Size(407, 27);
+            txt_name.TabIndex = 6;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Verdana", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(332, 51);
+            label7.Name = "label7";
+            label7.Size = new Size(96, 25);
+            label7.TabIndex = 4;
+            label7.Text = "Sign up";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Bradley Hand ITC", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(354, 75);
+            label1.TabIndex = 1;
+            label1.Text = "Mã hóa DES";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(75, 152);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(220, 127);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // btn_logintab
+            // 
+            btn_logintab.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_logintab.Location = new Point(129, 329);
+            btn_logintab.Name = "btn_logintab";
+            btn_logintab.Size = new Size(114, 40);
+            btn_logintab.TabIndex = 3;
+            btn_logintab.Text = "Login";
+            btn_logintab.UseVisualStyleBackColor = true;
+            btn_logintab.Click += btn_logintab_Click;
+            // 
+            // btn_signuptab
+            // 
+            btn_signuptab.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_signuptab.Location = new Point(129, 398);
+            btn_signuptab.Name = "btn_signuptab";
+            btn_signuptab.Size = new Size(114, 40);
+            btn_signuptab.TabIndex = 4;
+            btn_signuptab.Text = "Sign Up";
+            btn_signuptab.UseVisualStyleBackColor = true;
+            btn_signuptab.Click += btn_signuptab_Click;
+            // 
+            // TrangChu
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 192, 192);
+            ClientSize = new Size(1110, 635);
+            Controls.Add(btn_signuptab);
+            Controls.Add(btn_logintab);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Controls.Add(tabControl1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "TrangChu";
+            Text = "TrangChu";
+            Load += TrangChu_Load;
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label label4;
+        private Label label3;
+        private TextBox txt_password;
+        private TextBox txt_username;
+        private Label label2;
+        private Button btn_login;
+        private Button button1;
+        private Label label5;
+        private Label label6;
+        private TextBox txt_pass;
+        private TextBox txt_name;
+        private Label label7;
+        private Button btn_logintab;
+        private Button btn_signuptab;
+        private Label label8;
+        private TextBox txt_phone;
+    }
+}
---
+++ demo-des/TrangChu.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FireSharp;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+
+namespace demo_des
+{
+    public partial class TrangChu : Form
+    {
+        public TrangChu()
+        {
+            InitializeComponent();
+        }
+        // kết nối ứng dụng của mình với dịch vụ Firebase
+        IFirebaseConfig config = new FirebaseConfig()
+        {
+            AuthSecret = "C4n3cmu3NJVytNiNwmOt1lSiyknFm2GakZXJXsr5",
+            BasePath = "https://fir-des-647b5-default-rtdb.firebaseio.com/",
+        };
+        IFirebaseClient client;
+        des d = new des();
+        //key
+        ulong K1 = 0xF813;
+        ulong K2 = 0x5678;
+
+        private void btn_logintab_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedTab = tabPage1;
+        }
+
+        private void btn_signuptab_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedTab = tabPage2;
+        }
+
+        private void TrangChu_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                client = new FirebaseClient(config);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //kiểm tra nhập đầy đủ thông tin chưa
+            if (string.IsNullOrEmpty(txt_name.Text) || string.IsNullOrEmpty(txt_pass.Text) || string.IsNullOrEmpty(txt_phone.Text))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đù thông tin đề đăng kí tài khoản..");
+                return;
+            }
+            else
+            {
+                //cập nhật mã hóa password des
+                string encPass = d.mahoachuoi(txt_pass.Text.ToString(),K1,K2);
+
+                var dk = new dangki()
+                {
+                    Name = txt_name.Text,
+                    Phone = txt_phone.Text,
+                    Password = encPass,
+                };
+                //cập nhật dữ liệu lên cơ sở dữ liệu Firebase
+                FirebaseResponse response = client.Set("users/" + txt_phone.Text, dk);
+                dangki result = response.ResultAs<dangki>();
+                MessageBox.Show("Update thành công");
+            }
+        }
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txt_username.Text) || string.IsNullOrEmpty(txt_password.Text))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đù thông tin đề đăng kí tài khoản..");
+                return;
+            }
+            else
+            {
+                FirebaseResponse response = client.Get("users/");
+                Dictionary<string, dangki> result = response.ResultAs<Dictionary<string, dangki>>();    
+                foreach(var u in result)
+                {
+                    string username = u.Value.Name;
+                    string password = u.Value.Password;
+                    string decPass = d.giaimachuoi(password, K1, K2);
+                    if(txt_username.Text == username && txt_pass.Text == decPass) //them pass
+                    {
+                        Form1 f1 = new Form1();
+                        f1.ShowDialog();
+                    }
+                }
+            }
+        }
+    }
+}
