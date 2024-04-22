f082160e6ac24d3fbb338e77410a2aaa5ce3b0c8
cochecheee <buitien747@gmail.com>
2024-04-23 03:21:22 +0700

    model to call api from firebase

---
+++ demo-des/dangki.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo_des
+{
+    public class dangki
+    {
+        private string name;
+        private string phone;
+        private string password;
+
+        public string Name { get => name; set => name = value; }
+        public string Phone { get => phone; set => phone = value; }
+        public string Password { get => password; set => password = value; }
+    }
+}
